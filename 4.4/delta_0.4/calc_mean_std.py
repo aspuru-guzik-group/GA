003965de07578ae921d4db6f071e026ea72e705d
akshat998 <akshat998@gmail.com>
2020-01-06 14:30:29 -0500

    Experiment 4.4 added

---
+++ 4.4/delta_0.4/calc_mean_std.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 22 19:03:14 2019
+
+@author: akshat
+"""
+import numpy as np
+
+filename = './IMPROVEMENT.txt'
+
+with open(filename) as f:
+    content = f.readlines()
+content = [x.strip() for x in content] 
+content = [x[1:len(x)-1] for x in content]
+content = [float(x) for x in content]
+
+
+
+fail_cases = [x for x in content if x == 0]
+
+
+# Success percentage:
+success_P = 1 - (len(fail_cases) / len(content))
+print('Success percentage: ', success_P * 100)
+
+
+# Average Improvement and standard deviation:
+A = np.array(content)
+print('Mean: ', A.mean())
+print('Std : ', A.std())
+
+
+# Get the indices of the 3 best elements: 
+print(A.argsort()[-3:][::-1])
+# Output: [745 496  85]
